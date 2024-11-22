4262b7c193061e1bab5e791387f9c6e028bd414a
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2024-11-22 19:08:34 +0200

    eliminare comentarii

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,19 +14,15 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        // Validarea datelor introduse
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|min:6',
         ]);
 
-        // Încercarea autentificării
         if (Auth::attempt($request->only('email', 'password'))) {
-            // Redirecționează utilizatorul autentificat
             return redirect()->intended('/admin/dashboard');
         }
 
-        // Înapoi la formular cu eroare
         return back()->withErrors(['email' => 'Invalid credentials'])->withInput();
     }
 
