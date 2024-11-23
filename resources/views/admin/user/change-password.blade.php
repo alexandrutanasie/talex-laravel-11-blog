f059b7e17e9b7f3a612892791d97a8207c7ae82b
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2024-11-23 15:39:52 +0200

    add bootstrap to change password form

--- resources/views/admin/user/change-password.blade.php
+++ resources/views/admin/user/change-password.blade.php
@@ -3,23 +3,30 @@
 @section('title', 'Change password')
 
 @section('content')
-<form action="{{ route('admin.password.update') }}" method="POST">
+<form action="{{ route('admin.password.update') }}" method="POST" class="col-lg-4">
     @csrf
     @method('PUT')
 
-    <div>
-        <label for="new_password">New Password</label>
-        <input type="password" id="new_password" name="new_password" required>
+    <div class="form-group mb-3">
+        <label class="form-label" for="new_password">New Password</label>
+        <input class="form-control" type="password" id="new_password" name="new_password" required>
         @error('new_password')
             <span>{{ $message }}</span>
         @enderror
     </div>
 
-    <div>
-        <label for="new_password_confirmation">Confirm New Password</label>
-        <input type="password" id="new_password_confirmation" name="new_password_confirmation" required>
+    <div class="form-group mb-3">
+        <label class="form-label" for="new_password_confirmation">Confirm New Password</label>
+        <input class="form-control"  type="password" id="new_password_confirmation" name="new_password_confirmation" required>
     </div>
 
-    <button type="submit">Update Password</button>
+    <div class="form-group mb-3">
+        <button class="btn btn-primary btn-icon-split" type="submit">
+            <span class="icon text-white-50">
+                <i class="fas fa-plus-circle"></i>
+            </span>
+            <span class="text">Save</span>
+        </button>
+    </div>
 </form>
 @endsection
