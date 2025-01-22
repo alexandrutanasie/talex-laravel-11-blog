19b247d5daf3e4a8f049e72bcedc0a17c1d1dd07
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2025-01-22 17:24:05 +0200

    fix route login

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'index']);
-Route::get('/{url}', [FrontendController::class, 'main'])->where('url', '[a-zA-Z0-9_-]+')->name('main');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name("login");
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/{url}', [FrontendController::class, 'main'])->where('url', '[a-zA-Z0-9_-]+')->name('main');
 
 Route::prefix('admin')->as('admin.')->middleware('auth')->group(function () {
     Route::get('/dashboard',[AdminController::class, 'dashboard'])->name('dashboard');
